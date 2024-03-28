d169ca4305e27dc7d594e5e3bc3d5dbf731ba26e
nfq_mrojas <mario.rojas@nfq.es>
2024-03-28 22:22:18 +0100

    Extract Time related classes to pomodoro module

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,13 @@
+mod pomodoro;
+
 use std::fmt;
 use std::io::Write;
 use std::time::Duration;
 use async_std::task;
 use crossterm::{event, ExecutableCommand, QueueableCommand};
 use crossterm::event::{Event, KeyCode, KeyEvent};
+use crate::pomodoro::{PomodoroCommand, Time};
 
-#[derive(Debug)]
-struct Time {
-    hours: u8,
-    minutes: u8,
-    seconds: u8,
-}
-
-impl Time {
-    fn new() -> Time {
-        Time {
-            seconds: 0,
-            minutes: 0,
-            hours: 0,
-        }
-    }
-
-    fn increment_second(&mut self) {
-        self.seconds = (self.seconds + 1) % 60;
-        if self.seconds != 0 {
-            return;
-        }
-
-        self.minutes = (self.minutes + 1) % 60;
-        if self.minutes != 0 {
-            return;
-        }
-
-        self.hours += 1;
-    }
-}
-
-impl fmt::Display for Time {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{:02}:{:02}:{:02}",
-            self.hours, self.minutes, self.seconds
-        )
-    }
-}
-
-enum PomodoroCommand {
-    ClockIncrement,
-    KeyboardInput(char)
-}
 
 async fn clock_loop(tx: std::sync::mpsc::Sender<PomodoroCommand>) {
     loop {
@@ -106,47 +64,5 @@ pub fn run() -> Result<(), Box<dyn std::error::Error>> {
             }
             PomodoroCommand::ClockIncrement => time.increment_second(),
         }
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn increments_when_new_clock() {
-        let mut clock: Time = Time::new();
-
-        assert_eq!(clock.seconds, 0);
-        assert_eq!(clock.minutes, 0);
-        assert_eq!(clock.hours, 0);
-
-        clock.increment_second();
-        assert_eq!(clock.seconds, 1);
-        assert_eq!(clock.minutes, 0);
-        assert_eq!(clock.hours, 0);
-    }
-
-    #[test]
-    fn increments_when_to_minute() {
-        let mut clock: Time = Time::new();
-        clock.seconds = 59;
-
-        clock.increment_second();
-        assert_eq!(clock.seconds, 0);
-        assert_eq!(clock.minutes, 1);
-        assert_eq!(clock.hours, 0);
-    }
-
-    #[test]
-    fn increments_when_to_hour() {
-        let mut clock = Time::new();
-        clock.seconds = 59;
-        clock.minutes = 59;
-
-        clock.increment_second();
-        assert_eq!(clock.seconds, 0);
-        assert_eq!(clock.minutes, 0);
-        assert_eq!(clock.hours, 1);
     }
 }
